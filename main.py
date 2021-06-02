a82446f9b2131c4a9384360c2e58b878cb230352
Angel-Davila <a6519150005@utch.edu.mx>
2021-06-02 09:46:18 -0600

    post method created

--- main.py
+++ main.py
@@ -5,9 +5,32 @@ from flask_pymongo import pymongo
 import db_config as database
 
 app=Flask(__name__)
+api=Api(app)
 
-all_data = database.db.Badge.find()
+class Badge(Resource):
+     
+     def post(self):
+         database.db.Badge.insert_one(
+             {
+                 'header_img_url': request.json['header_img_url'],
+                 'profile_picture_url': request.json['profile_picture_url'],
+                 'name': request.json['name'],
+                 'age': request.json['age'],
+                 'city': request.json['city'],
+                 'followers': request.json['followers'],
+                 'like': request.json['like'],
+                 'post': request.json['post'],
+             }
+         )
 
+
+class AllBadge(Resource):
+    """" get all badges """
+    def get(self):
+        pass
+    
+api.add_resource(Badge, '/new')    
+    
 if __name__ == '__main__':
     app.run(load_dotenv=True)
     
