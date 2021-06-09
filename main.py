e3756b980825a7826dea05bb60e2331ed10c11bf
Angel-Davila <a6519150005@utch.edu.mx>
2021-06-09 11:50:09 -0600

    adding new route

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
 from flask import Flask, jsonify, request
-from flask.helpers import get_load_dotenv
 from flask_restful import Api, Resource, reqparse, abort
+from bson.json_util import dumps, ObjectId
 from flask_pymongo import pymongo
 import db_config as database
+
+#Resources
+from res.badge import Badge
+from res.badges import Badges
 
 app=Flask(__name__)
 api=Api(app)
 
-class Badge(Resource):
-     
-     def post(self):
-         database.db.Badge.insert_one(
-             {
-                 'header_img_url': request.json['header_img_url'],
-                 'profile_picture_url': request.json['profile_picture_url'],
-                 'name': request.json['name'],
-                 'age': request.json['age'],
-                 'city': request.json['city'],
-                 'followers': request.json['followers'],
-                 'like': request.json['like'],
-                 'post': request.json['post'],
-             }
-         )
+@app.route('/all/adults/')
+def get_adults():
+    response = list(database.db.Badges.find({'age': {"$gte": 21}}))
+    
+    for document in response:
+        document["_id"] = str(document['_id'])
+        
+    return jsonify(response)
+
+@app.route('/all/projections/')
+def get_name_and_age():
+    response = list(database.db.Badges.find({'age': {"$gte": 21}}, {'name':1, 'age':1}))
+    
+    for document in response:
+        document["_id"] = str(document['_id'])
+        
+    return jsonify(response)
+
+@app.route('/all/kids/')
+def get_kids():
+    response = list(database.db.Badges.find({'age': {"$lte": 12}}))
+    
+    for document in response:
+        document["_id"] = str(document['_id'])
+        
+    return jsonify(response)
 
 
-class AllBadge(Resource):
-    """" get all badges """
-    def get(self):
-        pass
-    
-api.add_resource(Badge, '/new')    
-    
+
+api.add_resource(Badge, '/new/','/<string:by>=<string:data>/')    
+api.add_resource(Badges, '/all/', '/delete/all/')
+api.add_resource(Posts, '/new/post/<string:_id>', '/posts/<string:_id>', '<string:_id>/<string:uuid>')
+
 if __name__ == '__main__':
     app.run(load_dotenv=True)
     
